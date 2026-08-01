5d852acb71157a92eeac6d54c354c2289af36343
Jashanjit Kaur <122778373+JashanjitKaur007@users.noreply.github.com>
2026-08-01 05:32:10 +0530

    Time: 22 ms (82.53%) | Memory: 48.2 MB (57.97%) - LeetSync

--- 3812-smallest-palindromic-rearrangement-i/smallest-palindromic-rearrangement-i.java
+++ 3812-smallest-palindromic-rearrangement-i/smallest-palindromic-rearrangement-i.java
@@ -3,64 +3,32 @@ class Solution {
         int n = s.length();
 
         int[] freq = new int[26];
-        for (char ch : s.toCharArray()) {
-            freq[ch - 'a']++;
+        for(char ch: s.toCharArray()){
+            freq[ch - 'a'] ++ ;
         }
 
         char[] ans = new char[n];
-        int k = -1;
-        int j = 0;
-
-        for (int i = 0; i < 26; i++) {
-
-            // ❌ Mistake: Only handling freq == 1 and freq == 2.
-            // ✅ Fix: Handle all frequencies using freq[i] / 2.
-            if (freq[i] % 2 == 1) {
-                k = i;
+        int k=-1;
+        int j=0;
+        for(int i=0; i<26; i++){
+            if(freq[i] % 2 == 1){
+                k=i;
             }
-
-            // ❌ Mistake:
-            // ans[j] = (char)('a' + freq[i]);
-            // Using frequency instead of character index.
-            // ✅ Fix: Use ('a' + i).
-            while (freq[i] >= 2) {
+            // if(freq[i]){
+            //     ans[j] = (char) ('a' + freq[i]);
+            //     ans[n-j-1] = (char) ('a' + freq[i]);
+            //     j++;
+            // }
+            while(freq[i] >= 2){
                 ans[j] = (char) ('a' + i);
-                ans[n - j - 1] = (char) ('a' + i);
-                freq[i] -= 2;
+                ans[n-j-1] = (char) ('a' + i);
                 j++;
+                freq[i] -= 2;
             }
         }
-
-        // ❌ Mistake:
-        // ans[n/2] = (char)('a' + freq[k]);
-        // Again using frequency instead of character index.
-        // ✅ Fix: Use ('a' + k).
-        if (k != -1) {
-            ans[n / 2] = (char) ('a' + k);
-        }
+        
+        if(k!= -1) ans[n/2] = (char) ('a' + k);
 
         return new String(ans);
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-

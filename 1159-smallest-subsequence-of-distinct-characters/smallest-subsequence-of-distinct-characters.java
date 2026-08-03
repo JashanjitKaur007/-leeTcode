188caafdb69e7aa65cc6cdd0a470599cd27d12be
Jashanjit Kaur <122778373+JashanjitKaur007@users.noreply.github.com>
2026-08-04 04:40:10 +0530

    Time: 3 ms (61.69%) | Memory: 42.8 MB (61.53%) - LeetSync

--- 1159-smallest-subsequence-of-distinct-characters/smallest-subsequence-of-distinct-characters.java
+++ 1159-smallest-subsequence-of-distinct-characters/smallest-subsequence-of-distinct-characters.java
@@ -1,40 +1,36 @@
 class Solution {
     public String smallestSubsequence(String s) {
 
+        char[] str = s.toCharArray();
+
         int[] freq = new int[26];
-        for(char ch: s.toCharArray()){
-            freq[ch-'a'] ++ ;
+        for(char ch: str){
+            freq[ch-'a']++;
         }
-        
+
         Stack<Character> st = new Stack<>();
 
         boolean[] present = new boolean[26];
-        Arrays.fill(present, false);
 
-        for(char ch: s.toCharArray()){
+        for(char ch: str){
             freq[ch-'a'] -- ;
-            if(present[ch-'a'] == true) continue;
+            if(present[ch-'a']) continue;
 
-            while(!st.isEmpty() && ch <= st.peek() && 
-                freq[st.peek()-'a'] > 0 && present[st.peek()-'a']){
-
+            while(!st.isEmpty() &&
+            ch <= st.peek()  &&  freq[st.peek() -'a'] > 0  &&  present[st.peek() -'a'] ){
                 present[st.peek()-'a'] = false;
                 st.pop();
             }
 
-            if(present[ch-'a'] == false){
+            if(!present[ch-'a']){
                 st.push(ch);
                 present[ch-'a'] = true;
             }
-
-            // freq[ch-'a'] -- ;
         }
 
         StringBuilder sb = new StringBuilder();
-        while(!st.isEmpty()){
-            sb.append(st.pop());
-        }
+        while(!st.isEmpty()) sb.append(st.pop());
 
         return sb.reverse().toString();
     }
-};
+}

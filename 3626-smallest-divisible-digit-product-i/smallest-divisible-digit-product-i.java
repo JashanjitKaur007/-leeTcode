ff341703abd639e5d7f5e28eda6591831415d309
Jashanjit Kaur <122778373+JashanjitKaur007@users.noreply.github.com>
2026-08-07 19:59:32 +0530

    Time: 1 ms (100.00%) | Memory: 42.3 MB (96.41%) - LeetSync

--- 3626-smallest-divisible-digit-product-i/smallest-divisible-digit-product-i.java
+++ 3626-smallest-divisible-digit-product-i/smallest-divisible-digit-product-i.java
@@ -1,15 +1,21 @@
 class Solution {
     public int smallestNumber(int n, int t) {
-        int q = n / 10, r = n % 10;
 
-        int req = t / gcd(q + (10 - q) / 10, t);
-        int nxt = ((r + req - 1) / req) * req;
-        int x = nxt - (nxt - 10) * (nxt / 10);
+        int ans = n;
 
-        return q * 10 + x;
-    }
+        while(true){
+            int prod = 1;
+            int num = ans;
+            while(num > 0){
+                prod *= num%10;
+                if(prod==0) return ans;
+                num/=10;
+            }
 
-    private int gcd(int a, int b) {
-        return (b == 0) ? a : gcd(b, a % b);
+            if(prod % t == 0) return ans;
+            else ans ++ ;
+        }
+        
+        // return 0;
     }
 }
